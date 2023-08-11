71109828507ad13f471c934c66acab3026fa7608
KozhaAkhmet <kozha24082001@gmail.com>
2023-08-11 19:46:53 +0000

    Parameters Homework part 1

--- src/my_cpp_pkg/src/number_publisher.cpp
+++ src/my_cpp_pkg/src/number_publisher.cpp
@@ -6,16 +6,24 @@ class NumberPublisher: public rclcpp::Node
 public:
     NumberPublisher(): Node("number_publisher")
     {
+        this->declare_parameter("number_to_publish", 2);
+        this->declare_parameter("publish_frequency", 1.0);
+
+        number_ = this->get_parameter("number_to_publish").as_int();
+        double publish_frequency = this->get_parameter("publish_frequency").as_double(); 
+
         publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number", 10);
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&NumberPublisher::publishNumber, this));
+        timer_ = this->create_wall_timer(std::chrono::milliseconds((int) (1000.0 / publish_frequency)) , std::bind(&NumberPublisher::publishNumber, this));
     }
 private:
     void publishNumber()
     {
-        auto number = example_interfaces::msg::Int64();
-        number.data = 1;
-        publisher_->publish(number);
+        auto msg = example_interfaces::msg::Int64();
+        msg.data = 1;
+        publisher_->publish(msg);
     }
+
+    int number_;
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
--- src/my_cpp_pkg/src/robot_news_station.cpp
+++ src/my_cpp_pkg/src/robot_news_station.cpp
@@ -6,6 +6,9 @@ class RobotNewsStationNode : public rclcpp::Node
 public:
     RobotNewsStationNode() : Node("robot_news_station"), robot_name_("R2D2")
     {
+        this->declare_parameter("robot_name", "R2D2");
+
+        robot_name_ = this->get_parameter("robot_name").as_string();
         publisher_ = this->create_publisher<example_interfaces::msg::String>("robot_news", 10);
         timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&RobotNewsStationNode::publishNews, this));
         RCLCPP_INFO(this->get_logger(), "Robot News Station has been started.");
