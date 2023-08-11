cb1fd51ab2a054968a80ed1d17aeb2b3867b1b1c
KozhaAkhmet <kozha24082001@gmail.com>
2023-08-11 16:49:52 +0000

    Studying ROS2 parameters

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -8,16 +8,20 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__("number_publisher")
+        self.declare_parameter("number_to_publish", 2) #second parameter is default value
+        self.declare_parameter("publish_frequency", 1.0)
 
-        self.number_ = 2
+        self.number_ = self.get_parameter("number_to_publish").value
+        self.publish_frequency_ = self.get_parameter("publish_frequency").value
+
         self.number_publisher_ = self.create_publisher(Int64, "number", 10)
-        self.number_timer_ = self.create_timer(1.0, self.publish_number)
+        self.number_timer_ = self.create_timer(1.0 / self.publish_frequency_, self.publish_number)
         self.get_logger().info("Number publisher has been started")
 
-        def publish_number(self):
-            msg = Int64()
-            msg.data = self.number_ 
-            self.number_publisher_publish(msg)
+    def publish_number(self):
+        msg = Int64()
+        msg.data = self.number_ 
+        self.number_publisher_.publish(msg)
 
 
 def main(args=None):
